96f12a6b5b78320037782c384804397273cf5133
Ryan Fuhrman <rsfuhrman10@gmail.com>
2019-09-13 11:37:57 -0400

    Added dynamic routs and dynamic navbar links

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React, { Component } from "react";
-import DogList from "./DogList";
 import { Switch, Route } from "react-router-dom";
+import DogList from "./DogList";
+import DogDetails from "./DogDetails";
+import Navbar from "./Navbar";
 import whiskey from "./images/whiskey.jpg";
 import tubby from "./images/tubby.jpg";
 import hazel from "./images/hazel.jpg";
@@ -43,13 +45,24 @@ class App extends Component {
   };
 
   render() {
+    const getDog = props => {
+      let name = props.match.params.name;
+      let currentDog = this.props.dogs.find(
+        dog => dog.name.toLowerCase() === name.toLowerCase()
+      );
+      return <DogDetails {...props} dog={currentDog} />;
+    };
     return (
-      <div className="App">
-        <Route
-          exact
-          path="/dogs"
-          render={routeProps => <DogList dogs={this.props.dogs} />}
-        />
+      <div>
+        <Navbar dogs={this.props.dogs} />
+        <Switch>
+          <Route
+            exact
+            path="/dogs"
+            render={routeProps => <DogList dogs={this.props.dogs} />}
+          />
+          <Route exact path="/dogs/:name" render={getDog} />
+        </Switch>
       </div>
     );
   }
---
+++ src/Navbar.js
@@ -0,0 +1,49 @@
+import React, { Component } from "react";
+import { withRouter, NavLink, Link } from "react-router-dom";
+
+class Navbar extends Component {
+  render() {
+    const dogLinks = this.props.dogs.map(dog => (
+      <li className="nav-item">
+        <NavLink
+          exact
+          to={`/dogs/${dog.name}`}
+          className="nav-link"
+          key={dog.name}
+        >
+          {dog.name}
+        </NavLink>
+      </li>
+    ));
+    return (
+      <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+        <Link to="/dogs" className="navbar-brand">
+          Dog App
+        </Link>
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-toggle="collapse"
+          data-target="#navbarNav"
+          aria-controlls="navbarNav"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarNav">
+          <ul className="navbar-nav">
+            <li className="nav-item">
+              <NavLink exact to="/dogs" className="nav-link">
+                Home
+              </NavLink>
+            </li>
+            {dogLinks}
+          </ul>
+        </div>
+      </nav>
+    );
+  }
+}
+
+export default withRouter(Navbar);
