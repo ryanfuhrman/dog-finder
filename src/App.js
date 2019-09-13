805a22578381829c7dc801b34d93063b6dbb4057
Ryan Fuhrman <rsfuhrman10@gmail.com>
2019-09-13 11:43:39 -0400

    Refactored the routes into their own component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,5 @@
 import React, { Component } from "react";
-import { Switch, Route } from "react-router-dom";
-import DogList from "./DogList";
-import DogDetails from "./DogDetails";
+import Routes from "./Routes";
 import Navbar from "./Navbar";
 import whiskey from "./images/whiskey.jpg";
 import tubby from "./images/tubby.jpg";
@@ -45,24 +43,10 @@ class App extends Component {
   };
 
   render() {
-    const getDog = props => {
-      let name = props.match.params.name;
-      let currentDog = this.props.dogs.find(
-        dog => dog.name.toLowerCase() === name.toLowerCase()
-      );
-      return <DogDetails {...props} dog={currentDog} />;
-    };
     return (
       <div>
         <Navbar dogs={this.props.dogs} />
-        <Switch>
-          <Route
-            exact
-            path="/dogs"
-            render={routeProps => <DogList dogs={this.props.dogs} />}
-          />
-          <Route exact path="/dogs/:name" render={getDog} />
-        </Switch>
+        <Routes dogs={this.props.dogs} />
       </div>
     );
   }
